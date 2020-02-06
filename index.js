99832904631f79530c27155f116d55a1cfbb0eae
LamNguyen98 <lamnguyen3298@gmail.com>
2020-02-06 21:21:32 +0700

    pug

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
-app.get('/', (request, response) => {
-    response.send('Hello World');
+app.set('view engine', 'pug');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        name: 'Lam'
+    });
+})
+
+app.get('/users', (req, res) => {
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Lam' },
+            { id: 2, name: 'Linh' }
+        ]
+    });
 })
 
 app.listen(port, () => {
