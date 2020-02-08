c0c276a660454e68290e3f272688ee99b3a32056
LamNguyen98 <lamnguyen3298@gmail.com>
2020-02-08 12:31:06 +0700

    searchbar

--- index.js
+++ index.js
@@ -5,21 +5,32 @@ let port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views')
 
+let users = [
+    { id: 1, name: 'Lam' },
+    { id: 2, name: 'Linh' }
+];
+
 app.get('/', (req, res) => {
     res.render('index', {
         name: 'Lam'
     });
-})
+});
 
 app.get('/users', (req, res) => {
-    res.render('users/index', {
-        users: [
-            { id: 1, name: 'Lam' },
-            { id: 2, name: 'Linh' }
-        ]
+    res.render('users/index', { users: users })
+});
+
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    let matchedUser = users.filter((user) => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
-})
+
+    res.render('users/index', {
+        users: matchedUser
+    });
+});
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
-})
+});
