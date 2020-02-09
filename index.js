05aed1dd8549be19bc45dcfc95e188378acfcece
LamNguyen98 <lamnguyen3298@gmail.com>
2020-02-09 13:57:20 +0700

    POST method

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
 let port = 3000;
 
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let users = [
     { id: 1, name: 'Lam' },
@@ -25,11 +30,20 @@ app.get('/users/search', (req, res) => {
     let matchedUser = users.filter((user) => {
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
-
     res.render('users/index', {
         users: matchedUser
     });
 });
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create')
+});
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+    res.redirect('/users')
+})
+
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
