a92223657f5a7c8ad70189379c755441ff819d2e
LamNguyen98 <lamnguyen3298@gmail.com>
2020-02-18 14:11:17 +0700

    pages

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,13 @@
 const db = require('../db');
 
 module.exports.index = (req, res) => {
+    let page = parseInt(req.query.page) || 1;
+    let perPage = 8;
+
+    let start = (page -1)* perPage;
+    let end = page*perPage;
+
     res.render('products/index', {
-        products: db.get('products').value()
+        products: db.get('products').value().slice(start, end)
     })
 };
